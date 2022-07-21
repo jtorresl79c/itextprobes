29b2fccd8bd45c5f41d14f81333fe831972ec364
jtorresl79c <83847060+jtorresl79c@users.noreply.github.com>
2022-07-21 16:37:22 -0700

    Rectangle example finalizado

--- stationconsoleapp/RectangleExample.cs
+++ stationconsoleapp/RectangleExample.cs
@@ -32,16 +32,36 @@ namespace stationconsoleapp
             var fos = new FileStream(dest, FileMode.Create);
             var writer = new PdfWriter(fos);
             var pdf = new PdfDocument(writer);
-            var ps = PageSize.A4.Rotate();
+            //var ps = PageSize.A4.Rotate();
+            var ps = PageSize.LETTER;
             var page = pdf.AddNewPage(ps);
 
             var canvas = new PdfCanvas(page);
 
 
-            Rectangle rect = new Rectangle(0, 300, 400, 50);
-            Paragraph p = new Paragraph("REVISO");
+            Rectangle rect = new Rectangle(ps.GetWidth() - 180, 40, 130, 80);
+            
+            Paragraph p = new Paragraph().SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER).SetFontSize(6);
+            PdfFont boldFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
 
-            new Canvas(canvas, rect).Add(p);
+
+            Canvas x = new Canvas(canvas, rect);
+            p.Add(new Text("REVISÓ").SetFont(boldFont));
+            p.Add(new Text("\n"));
+            p.Add(new Text("\n"));
+            p.Add(new Text("\n"));
+            p.Add(new Text("\n"));
+            p.Add(new Text("JUAN MANUEL SANDOVAL RODRIGUEZ").SetFont(boldFont));
+            p.Add(new Text("\n"));
+            p.Add(new Text("Verificador Inspector de Protección Civil"));
+            p.Add(new Text("\n"));
+            p.Add(new Text("Departamento de Verificaciones"));
+            x.Add(p);
+
+            //x.Add(new Paragraph("REVISO"));
+            //x.Add(new Text("/n"));
+            //x.Add(new Text("/n"));
+            //x.Add(new Paragraph("REVISO2"));
 
             canvas.Rectangle(rect);
             //canvas.Stroke();
